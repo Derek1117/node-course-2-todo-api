7b05a1e322b969f24b2721fbb48e46d1444347d3
Derek1117 <derekl1117@yahoo.com.hk>
2016-12-02 16:30:12 +0800

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,10 +9,13 @@ const server_1 = require("./../server");
 const todo_1 = require("./../models/todo");
 const todos = [{
         _id: new mongodb_1.ObjectID(),
-        text: 'First test todo'
+        text: 'First test todo',
+        completed: false
     }, {
         _id: new mongodb_1.ObjectID(),
-        text: 'Second test todo'
+        text: 'Second test todo',
+        completed: true,
+        completedAt: 123
     }];
 beforeEach((done) => {
     todo_1.Todo.remove({}).then(() => {
@@ -121,6 +124,42 @@ describe('DELETE /todos/:id', () => {
         request(server_1.app)
             .delete(`/todos/aaa`)
             .expect(404)
+            .end(done);
+    });
+});
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+        request(server_1.app)
+            .patch(`/todos/${hexId}`)
+            .send({
+            completed: true,
+            text
+        })
+            .expect(200)
+            .expect((res) => {
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completedAt).toBeA('number');
+        })
+            .end(done);
+    });
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'This should be the new text';
+        request(server_1.app)
+            .patch(`/todos/${hexId}`)
+            .send({
+            completed: false,
+            text
+        })
+            .expect(200)
+            .expect((res) => {
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completedAt).toNotExist();
+        })
             .end(done);
     });
 });
