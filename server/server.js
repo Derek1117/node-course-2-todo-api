33cc1ddcaa364283fb8bf564c8f3f60aec8dee30
Derek1117 <derekl1117@yahoo.com.hk>
2016-12-07 15:45:58 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -121,23 +121,17 @@ app.post('/users', (req, res) => {
     })
 });
 
-//
-// var authenticate = (req, res, next) => {
-//     var token = req.header('x-auth');
-//
-//     User.findByToken(token).then((user) => {
-//         if (!user) {
-//             // res.status(401).send();
-//             return Promise.reject();
-//         }
-//
-//         res.user = user;
-//         req.token = token;
-//         next();
-//     }).catch((e) => {
-//         res.status(401).send();
-//     });
-// };
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
